f6d3e3b351626cd7e618c5b4c524042fbb270064
Masahiro Takata <takata005581016@gmail.com>
2020-05-20 22:33:10 +0900

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by 高田将弘 on 2020/05/20.
+//  Copyright © 2020 高田将弘. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
