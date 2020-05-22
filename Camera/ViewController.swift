cfc33676be55e29fda191fde7238ae8bdc23466a
Masahiro Takata <takata005581016@gmail.com>
2020-05-22 23:17:23 +0900

    storyboardにカメラボタンとimageViewの追加

--- Camera/ViewController.swift
+++ Camera/ViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func launchCamera(_ sender: UIBarButtonItem) {
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-    func Log(){
-        print("テスト");
-        print("高田です！よろしく！！");
     }
 }
 
