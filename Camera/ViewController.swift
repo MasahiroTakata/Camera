256cb379223f63c24e0b93921ea9daa86bb17749
Masahiro Takata <takata005581016@gmail.com>
2020-05-24 22:28:16 +0900

    カメラアプリの実機での起動及び、写真の保存

--- Camera/ViewController.swift
+++ Camera/ViewController.swift
@@ -8,10 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    // カメラアプリ起動
     @IBAction func launchCamera(_ sender: UIBarButtonItem) {
+        let camera = UIImagePickerController.SourceType.camera
+        
+        if UIImagePickerController.isSourceTypeAvailable(camera){
+            let picker = UIImagePickerController()
+            picker.sourceType = camera
+            picker.delegate = self
+            self.present(picker, animated: true)
+        }
+    }
+
+    // 撮影終了時（撮影が終了すると自動的に呼び出されるメソッド）
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        self.imageView.image = image
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil) // 写真を保存する
+        self.dismiss(animated: true)
     }
 
     override func viewDidLoad() {
